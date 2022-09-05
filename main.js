ddee9633d33164dca8d78805a693fcd8f0825af7
Maurício Oliveira Maciel Pacheco <107279794+devmauricio1991@users.noreply.github.com>
2022-09-05 19:21:17 -0300

    adicionando primeira função

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+//true = ( > que 5mm de diferença no halo em comparação ao mero)
+//false = (não possio diferença em relação ao mero)
 
+var mero = parseInt(prompt("Digite o halo do Meropenem"));
+var boronico = parseInt(prompt("Digite o halo do meropenem com ac.Boronico"));
+var edta = parseInt(prompt("Digite o halo do meropenem com EDTA"));
+var cloxa = parseInt(prompt("Digite o halo do meropenem com cloxa"));
+
+function verificainibidores (){
+    if ((mero-boronico) <= 5 && ((mero-edta) <= 5 && ((mero - cloxa) <=5){
+        `O mecanismo mudança permeabilidade`
+    }
+}
