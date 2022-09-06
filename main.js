c64432e38f078e244e96d3e305cf02e245bf78d5
Maurício Oliveira Maciel Pacheco <107279794+devmauricio1991@users.noreply.github.com>
2022-09-05 22:39:33 -0300

    commit nova versão

--- main.js
+++ main.js
@@ -2,17 +2,21 @@
 //false = (não possio diferença em relação ao mero)
 
 let mero = parseInt(prompt("Digite o halo do Meropenem"));
-let boronico = ((parseInt(prompt("Digite o halo do meropenem com ac.Boronico"))) - mero);
-let edta = ((parseInt(prompt("Digite o halo do meropenem com EDTA"))) - mero);
-let cloxa = ((parseInt(prompt("Digite o halo do meropenem com cloxa"))) - mero);
+let boronico = parseInt(prompt("Digite o halo do meropenem com ac.Boronico"));
+let edta = parseInt(prompt("Digite o halo do meropenem com EDTA"))
+let cloxa = parseInt(prompt("Digite o halo do meropenem com cloxa"))
+
+console.log(mero);
+console.log(boronico);
+console.log(edta);
+console.log(cloxa);
 
 function verificainibidores (mero,boronico,edta,cloxa){
-    if (boronico <= 5 && edta <= 5 && cloxa <=5)
-        console.log(`Mecanismo: Permeabilidade`);
-        else{
-            (boronico >= 5 && edta <= 5 && cloxa <=5)
-        console.log(`Mecanismo: KPC`)
-        }
+    if ((boronico - mero) <= 5 && (edta - mero) <= 5 && (cloxa - mero) <=5)
+        console.log(`Mecanismo: Permeabilidade`)else{
+            ((boronico - mero) >= 5 && (edta - mero) <= 5 && (cloxa - mero) <=5)
+            console.log(`Mecanismo: KPC`)
+            }
     }
 
 verificainibidores();
